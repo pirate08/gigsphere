e637fc28152df906664367e72739a89df174c5e4
Mayukh <mayukhdebgoswami82@gmail.com>
2025-10-25 17:28:08 +0530

    Profile base code is ready...

---
+++ src/app/client-dashboard/profile/page.tsx
@@ -0,0 +1,12 @@
+import ProfileUI from '@/components/Profile';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <ProfileUI />
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileUI = () => {
+  return (
+    <div className='bg-black text-white h-screen w-full flex justify-center items-center text-2xl'>
+      <h1>Hello guyz, your profile is ready..and is coming soon..</h1>
+    </div>
+  );
+};
+
+export default ProfileUI;
