ffcdded04d2d714272e560bb436a750663862f7f
Mayukh <mayukhdebgoswami82@gmail.com>
2025-11-06 11:59:22 +0530

    Base code is ready of find-work page...

---
+++ src/app/find-work/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import FindWorkUi from '@/components/FindWork';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
+
+const FindWork = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = getCookie('user_token');
+    const role = getCookie('user_role');
+
+    if (!token || !role) {
+      router.push('/login');
+      return;
+    }
+
+    if (role !== 'freelancer') {
+      router.push('/login');
+      return;
+    }
+  }, [router]);
+
+  return (
+    <div>
+      <FindWorkUi />
+    </div>
+  );
+};
+
+export default FindWork;
---
+++ src/components/FindWork.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FindWorkUi = () => {
+  return (
+    <div className='h-screen w-full flex px-10 py-10 items-start bg-black text-white'>
+      Hello Find Work!
+    </div>
+  );
+};
+
+export default FindWorkUi;
