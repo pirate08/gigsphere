6b6f558281efe4e90163ecafcd571e9581b4f299
Mayukh <mayukhdebgoswami82@gmail.com>
2025-11-12 23:54:24 +0530

    Portfolio Model icon added...

--- src/ui/AddNewPortfolioProject.tsx
+++ src/ui/AddNewPortfolioProject.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { LiaCertificateSolid } from 'react-icons/lia';
+import { GoProjectRoadmap } from 'react-icons/go';
 
 // Define the expected shape of an experience item
 export interface PortfolioItem {
@@ -73,9 +73,9 @@ const AddNewPortfolioProject: React.FC<AddNewPortfolioProps> = ({
       <div className='bg-gray-800 border border-gray-700 rounded-xl shadow-2xl w-full max-w-lg max-h-[100vh] p-6 sm:p-8 transform transition-all duration-300 scale-100'>
         <h2 className='text-2xl font-bold text-white mb-6 border-b border-gray-700 pb-3 flex items-center gap-2'>
           <span>
-            <LiaCertificateSolid />
+            <GoProjectRoadmap />
           </span>
-          Add New Certificate
+          Add New Project
         </h2>
 
         <form onSubmit={handleSubmit} className='space-y-5'>
@@ -113,7 +113,9 @@ const AddNewPortfolioProject: React.FC<AddNewPortfolioProps> = ({
 
           {/* Date */}
           <div>
-            <label htmlFor='description' className='block text-sm text-gray-300 mb-1'>
+            <label
+              htmlFor='description'
+              className='block text-sm text-gray-300 mb-1'>
               Description <span className='text-red-500'>*</span>
             </label>
             <textarea
